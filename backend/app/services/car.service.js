2c7df459baa8b695288c6c625b2ff25a887d24d5
shankaranarayanan712 <shankarhari712@gmail.com>
2021-11-01 13:56:04 +0530

    Defensive check

--- backend/app/services/car.service.js
+++ backend/app/services/car.service.js
@@ -2,56 +2,58 @@ const carExternalAPI = require('./carExternalService');
 
 const getCarDetails = async ($) => {
   try {
-    const modelArray = [];
-    const BASE_URL = 'https://en.wikipedia.org';
-    const table = $.html('.wikitable > tbody > tr > th > i > a');
-    // eslint-disable-next-line consistent-return
-    // As the html response is huge, we would have to wait for the scrambler to operate
-    return new Promise((fulfill) => {
-      $(table).each(async (index, value) => {
-        const modelDet = {};
-        const modelDetails = await carExternalAPI.fetchHTML(`${BASE_URL}${$(value).attr('href')}`);
-        if (modelDetails) {
-          const manufacturer = modelDetails.html('.hproduct .infobox-above');
-          const image = modelDetails.html('.image');
-          const production = modelDetails.html('.infobox-data');
-          // eslint-disable-next-line func-names
-          modelDetails(manufacturer).each(function () {
-            modelDet.name = modelDetails(this).text() ? modelDetails(this).text() : '';
-          });
-          modelDet.link = modelDetails(image).children().first()[0].attribs.src;
-          modelDet.body = modelDetails(production).text();
-          if (production) {
-            const regex1 = /19/;
-            const regex2 = /18/;
-            const regex3 = /17/;
-            // The production year is jumbed across the html, hence do a regex search to find out
-            let search = production.match(regex1);
-            if (!search) {
-              search = production.match(regex2);
+    if ($) {
+      const modelArray = [];
+      const BASE_URL = 'https://en.wikipedia.org';
+      const table = $.html('.wikitable > tbody > tr > th > i > a');
+      // eslint-disable-next-line consistent-return
+      // As the html response is huge, we would have to wait for the scrambler to operate
+      return new Promise((fulfill) => {
+        $(table).each(async (index, value) => {
+          const modelDet = {};
+          const modelDetails = await carExternalAPI.fetchHTML(`${BASE_URL}${$(value).attr('href')}`);
+          if (modelDetails) {
+            const manufacturer = modelDetails.html('.hproduct .infobox-above');
+            const image = modelDetails.html('.image');
+            const production = modelDetails.html('.infobox-data');
+            // eslint-disable-next-line func-names
+            modelDetails(manufacturer).each(function () {
+              modelDet.name = modelDetails(this).text() ? modelDetails(this).text() : '';
+            });
+            modelDet.link = modelDetails(image).children().first()[0].attribs.src;
+            modelDet.body = modelDetails(production).text();
+            if (production) {
+              const regex1 = /19/;
+              const regex2 = /18/;
+              const regex3 = /17/;
+              // The production year is jumbed across the html, hence do a regex search to find out
+              let search = production.match(regex1);
               if (!search) {
-                search = production.match(regex3);
+                search = production.match(regex2);
+                if (!search) {
+                  search = production.match(regex3);
+                }
+              }
+              if (search && search.index) {
+                // modelDet.productionDate
+                let symbols = '<';
+                const temp = production.substring(search.index, search.index + 15);
+                symbols = temp.match(symbols);
+                if (symbols) {
+                  modelDet.production = temp.substring(0, symbols.index);
+                }
               }
             }
-            if (search && search.index) {
-              // modelDet.productionDate
-              let symbols = '<';
-              const temp = production.substring(search.index, search.index + 15);
-              symbols = temp.match(symbols);
-              if (symbols) {
-                modelDet.production = temp.substring(0, symbols.index);
-              }
+            if (modelDet && modelDet.name && modelDet.production) {
+              modelArray.push(modelDet);
+            }
+            if (modelArray && modelArray.length === 200) {
+              fulfill(modelArray);
             }
           }
-          if (modelDet && modelDet.name && modelDet.production) {
-            modelArray.push(modelDet);
-          }
-          if (modelArray && modelArray.length === 200) {
-            fulfill(modelArray);
-          }
-        }
+        });
       });
-    });
+    }
   } catch (err) {
     console.log('Err', err);
   }
